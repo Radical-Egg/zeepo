b0fffd79231bb165d805b390726670e54a404050
Radical Egg <luther.jaymen@gmail.com>
2023-02-21 13:37:14 -0500

    fixed reload function

--- cmd/zeepo/zeepo.go
+++ cmd/zeepo/zeepo.go
@@ -7,23 +7,26 @@ import (
 )
 
 func main() {
+	pages := tview.NewPages()
 	grid := tui.GenerateColorGrid()
 	app := tview.NewApplication()
 
+	pages.AddPage("Color Grid", grid, true, true)
+
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
-		case tcell.KeyEnter:
-			grid = tui.GenerateColorGrid()
-			if err := app.SetRoot(grid, true).EnableMouse(true).Run(); err != nil {
-				panic(err)
+			case tcell.KeyEnter:
+				grid = tui.GenerateColorGrid()
+				pages.RemovePage("Color Grid")
+				pages.AddPage("Color Grid", grid, true, true)
+				pages.SwitchToPage("Color Grid")
+			case tcell.KeyCtrlC, tcell.KeyEsc:
+				app.Stop()
 			}
-		case tcell.KeyCtrlC:
-			app.Stop()
-		}
 		return event
 	})
 
-	if err := app.SetRoot(grid, true).EnableMouse(true).Run(); err != nil {
+	if err := app.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
 }
